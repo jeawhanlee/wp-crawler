a954d754c0870e3fd28e1786c27f040d4613734d
Michael Lee <michaelleemichaellee408@gmail.com>
2022-02-25 18:51:20 +0100

    added header to plugin

---
+++ wp-crawler.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: WP-Crawler
+Plugin URI: http://wordpress.org
+Description: A Wordpress plulgin for crawling and extracting links of homepage.
+Version: 1.0
+Author: Michael Lee
+Author URI: http://wordpress.org
+License: GPLv2
+*/
+ 
