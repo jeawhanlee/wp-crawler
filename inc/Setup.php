0dd185505b70e9a9d54be997b5246ef06d913769
Michael Lee <michaelleemichaellee408@gmail.com>
2022-03-07 18:18:46 +0100

    phpcs fix

--- inc/Setup.php
+++ inc/Setup.php
@@ -4,74 +4,74 @@ namespace WP_Crawler;
 
 class Setup {
 
-    /**
-     * Crawl Object
-     *
-     * @var [type]
-     */
-    private $wpc_crawl;
+	/**
+	 * Crawl Object
+	 *
+	 * @var [type]
+	 */
+	private $wpc_crawl;
 
-    /**
-     * plugin assets ( Styles and Scripts)
-     *
-     * @var array
-     */
-    private $assets = array();
+	/**
+	 * plugin assets ( Styles and Scripts)
+	 *
+	 * @var array
+	 */
+	private $assets = array();
 
-    /**
-     * pass crawl object
-     *
-     * @param [type] $wpc_crawl
-     */
-    public function __construct( $wpc_crawl ){
+	/**
+	 * pass crawl object
+	 *
+	 * @param [type] $wpc_crawl
+	 */
+	public function __construct( $wpc_crawl ) {
 
-        $this->wpc_crawl = $wpc_crawl;
+		$this->wpc_crawl = $wpc_crawl;
 
-    }
+	}
 
-   /**
+	/**
 	 * Require plugin cdn styles and scripts
 	 *
 	 * @return void
 	 */
-    public function load_assets() {
-        /**
-         * Assets (Styles & Scripts) for plugin
-         */
-        $this->assets = array(
-            /**
-             * Register Bootstrap CSS
-             */
-            'bootstrap-wpc_style'  => 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
+	public function load_assets() {
+		/**
+		 * Assets (Styles & Scripts) for plugin
+		 */
+		$this->assets = array(
+			/**
+			 * Register Bootstrap CSS
+			 */
+			'bootstrap-wpc_style'  => 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
 
-            /**
-             * Register local styling
-             */
-            'wpc-style_style'      => plugins_url() . '/' . WPC_APP . '/inc/Views/assets/css/wpc.style.css',
+			/**
+			 * Register local styling
+			 */
+			'wpc-style_style'      => plugins_url() . '/' . WPC_APP . '/inc/Views/assets/css/wpc.style.css',
 
-            /**
-             * Register Vuejs
-             */
-            'vue-wpc_script'       => 'https://cdn.jsdelivr.net/npm/vue@2.6.14',
+			/**
+			 * Register Vuejs
+			 */
+			'vue-wpc_script'       => 'https://cdn.jsdelivr.net/npm/vue@2.6.14',
 
-            /**
-            * Register Axios
-            */
-            'axios-wpc_script'     => 'https://cdnjs.cloudflare.com/ajax/libs/axios/0.26.0/axios.min.js',
+			/**
+			* Register Axios
+			*/
+			'axios-wpc_script'     => 'https://cdnjs.cloudflare.com/ajax/libs/axios/0.26.0/axios.min.js',
 
-            /**
-             * Register local scripting
-             */
-            'wpc-script_script'    => plugins_url() . '/' . WPC_APP . '/inc/Views/assets/js/wpc.script.js',
+			/**
+			 * Register local scripting
+			 */
+			'wpc-script_script'    => plugins_url() . '/' . WPC_APP . '/inc/Views/assets/js/wpc.script.js',
 
-            /**
-             * Register Bootstrap JS
-             */
-            'bootstrap-wpc_script' => 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
-        );
+			/**
+			 * Register Bootstrap JS
+			 */
+			'bootstrap-wpc_script' => 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
+		);
 
 		$this->wpc_enqueue_assets();
-    }
+	}
 
 	/**
 	 * add plugin section to menu
@@ -109,25 +109,25 @@ class Setup {
 	 */
 	public function wpc_setup() {
 
-        /**
-         * Load assets
-         */
-        add_action( 'admin_enqueue_scripts', array( $this, 'load_assets' ) ); 
+		/**
+		 * Load assets
+		 */
+		add_action( 'admin_enqueue_scripts', array( $this, 'load_assets' ) );
 
 		/**
 		 * add plugin menu
 		 */
 		add_action( 'admin_menu', array( $this, 'create_menu' ) );
 
-        /**
-         * register CRAWLER routes
-         */
-        add_action( 'rest_api_init', array( $this->wpc_crawl, 'register_routes' ) );
+		/**
+		 * register CRAWLER routes
+		 */
+		add_action( 'rest_api_init', array( $this->wpc_crawl, 'register_routes' ) );
 
-        /**
-         * Cron to crawl
-         */
-        add_action( 'wpc_page_crawl', array( $this->wpc_crawl, 'crawl' ) );
+		/**
+		 * Cron to crawl
+		 */
+		add_action( 'wpc_page_crawl', array( $this->wpc_crawl, 'crawl' ) );
 
 		/**
 		 * Set cron to crawl
@@ -141,34 +141,34 @@ class Setup {
 		}
 	}
 
-    /**
-     * Enqueue assets (Styles & Scripts)
-     *
-     * @param array $assets
-     * @return void
-     */
-    private function wpc_enqueue_assets() {
-        /**
+	/**
+	 * Enqueue assets (Styles & Scripts)
+	 *
+	 * @param array $assets
+	 * @return void
+	 */
+	private function wpc_enqueue_assets() {
+		/**
 		 * Loop through the assets array
 		 */
 		foreach ( $this->assets as $id => $src ) {
 
-            /**
-            * Register assets
-            */
-           $asset_type = explode( '_', $id )[1];
+			/**
+			* Register assets
+			*/
+			$asset_type = explode( '_', $id )[1];
 
-           if ( $asset_type == 'style' ) {
-               /**
-                * enqueue styles
-                */
-               wp_enqueue_style( $id, $src );
-           } elseif ( $asset_type == 'script' ) {
-               /**
-                * enqueue scripts at the footer
-                */
-               wp_enqueue_script( $id, $src, null, null, true );
-           }
-       }
-    }
+			if ( $asset_type == 'style' ) {
+				/**
+				 * enqueue styles
+				 */
+				wp_enqueue_style( $id, $src );
+			} elseif ( $asset_type == 'script' ) {
+				/**
+				 * enqueue scripts at the footer
+				 */
+				wp_enqueue_script( $id, $src, null, null, true );
+			}
+		}
+	}
 }
